d7a0a010ee15eb6c39ce658fd9cde554f0554b6e
Mateusz Groszewski <neonowa.vi@gmail.com>
2024-05-11 15:02:47 +0200

    feat: added updateData and deleteData functions

--- src/dbcontroller.py
+++ src/dbcontroller.py
@@ -8,11 +8,11 @@ class DBController:
 
     @staticmethod
     def createTable() -> None:
-        conn = sqlite3.connect('storage.db')
+        dbConnection = sqlite3.connect('storage.db')
 
-        cur = conn.cursor()
+        dbCursor = dbConnection.cursor()
 
-        cur.execute("""CREATE TABLE IF NOT EXISTS storage(
+        dbCursor.execute("""CREATE TABLE IF NOT EXISTS storage(
         ID INTEGER PRIMARY KEY AUTOINCREMENT,
         SERVICE TEXT,
         MAIL TEXT,
@@ -20,37 +20,49 @@ class DBController:
         PWD TEXT
         )""")
 
-        conn.commit()
-        conn.close()
+        dbConnection.commit()
+        dbConnection.close()
 
     @staticmethod
     def showAll() -> None:
-        conn = sqlite3.connect('storage.db')
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM storage")
-        table: list = cur.fetchall()
+        dbConnection = sqlite3.connect('storage.db')
+        dbCursor = dbConnection.cursor()
+        dbCursor.execute("SELECT * FROM storage")
+        table: list = dbCursor.fetchall()
         for row in table:
             print(f'ID: {row[0]:<3} | Service:  {row[1]:<20} | Mail: {row[2]:<20} | Login: {row[3]:<20} | Pwd: {row[4]:<20}')
-        conn.commit()
-        conn.close()
+        dbConnection.commit()
+        dbConnection.close()
 
     @staticmethod
     def insertData(service: str, mail: str, login: str, current: str) -> None:
-        conn = sqlite3.connect('storage.db')
-        cur = conn.cursor()
+        dbConnection = sqlite3.connect('storage.db')
+        dbCursor = dbConnection.cursor()
 
-        cur.execute(f"INSERT INTO storage (SERVICE, MAIL, LOGIN, PWD) VALUES ('{service}', '{mail}', '{login}', '{current}')")
-        conn.commit()
-        conn.close()
-
-    @staticmethod
-    def updateData() -> None:
-        conn = sqlite3.connect('storage.db')
-        cur = conn.cursor()
-        cur.execute(f"UPDATE storage SET PWD = ''")
-        conn.commit()
-        conn.close()
+        dbCursor.execute(f"INSERT INTO storage (SERVICE, MAIL, LOGIN, PWD) VALUES ('{service}', '{mail}', '{login}', '{current}')")
+        dbConnection.commit()
+        dbConnection.close()
 
     @staticmethod
-    def deleteData(service: str, mail: str, login: str) -> None:
-        raise NotImplemented
+    def updateData(newData: str, itemID: int, dataToChange: int) -> None:
+        dbConnection = sqlite3.connect('storage.db')
+        dbCursor = dbConnection.cursor()
+        if dataToChange == 1:
+            dbCursor.execute(f"UPDATE storage SET SERVICE = '{newData}' WHERE ID = {itemID}")
+        elif dataToChange == 2:
+            dbCursor.execute(f"UPDATE storage SET MAIL = '{newData}' WHERE ID = {itemID}")
+        elif dataToChange == 3:
+            dbCursor.execute(f"UPDATE storage SET LOGIN = '{newData}' WHERE ID = {itemID}")
+        else:
+            dbCursor.execute(f"UPDATE storage SET PWD = '{newData}' WHERE ID = {itemID}")
+        dbConnection.commit()
+        dbConnection.close()
+
+    @staticmethod
+    def deleteData(itemID: int) -> None:
+        dbConnection = sqlite3.connect('storage.db')
+        dbCursor = dbConnection.cursor()
+        dbCursor.execute(f"DELETE FROM storage WHERE ID = {itemID}")
+        dbConnection.commit()
+        dbConnection.close()
+
